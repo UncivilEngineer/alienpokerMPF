cb11dfb12628c32abb84cccff5113a2ab74006e0
UncivilEngineer <whitten.alan@gmail.com>
2017-04-28 20:39:55 -0700

    Bonus mode complete!

---
+++ modes/bonus/code/bonus.py
@@ -0,0 +1,169 @@
+from mpf.core.mode import Mode
+import random
+
+#this is the bonus mode
+#it is intended to be used to track the bonus lights
+#and at the end of a ball, award the bonus
+#it will also track the bonus multiplier
+
+class bonus(Mode):
+  def mode_init(self):
+    self.log.info('bonus python code initalized')
+
+  def mode_start(self, **kwargs):
+    #initalized bonus Values
+    self.player.bonus = 0
+    self.player.bonus_x = 1
+    self.player.ll_joker_on = False
+    self.player.ml_joker_on = False
+    self.player.ul_joker_on = False
+    self.player.r_joker_on = False
+
+    #the events the mode is watching for
+    self.add_mode_event_handler("s_lower_left_joker_active", self.ll_joker_score)
+    self.add_mode_event_handler("s_middle_left_joker_active", self.ml_joker_score)
+    self.add_mode_event_handler("s_top_joker_active", self.ul_joker_score)
+    self.add_mode_event_handler("s_right_Joker_active", self.r_joker_score)
+    self.add_mode_event_handler("player_bonus", self.update_bonus_lights)
+    #this routine will handle setting up the lighted jokers
+    self.joker_setup()
+
+  def ll_joker_score(self, **kwargs):
+      if self.player.ll_joker_on == True:
+        self.player.bonus_x += 1
+        self.player.bonus += 1000
+        self.update_bonus_lights()
+        self.joker_setup()
+        self.machine.events.post('bonus_x_increase')
+      else:
+          self.player.score += 500
+
+
+  def ml_joker_score(self, **kwargs):
+      if self.player.ml_joker_on == True:
+        self.player.bonus_x += 1
+        self.player.bonus += 1000
+        self.update_bonus_lights()
+        self.joker_setup()
+        self.machine.events.post('bonus_x_increase')
+      else:
+        self.player.score += 500
+
+  def ul_joker_score(self, **kwargs):
+      if self.player.ul_joker_on == True:
+        self.player.bonus_x += 1
+        self.player.bonus += 1000
+        self.update_bonus_lights()
+        self.joker_setup()
+        self.machine.events.post('bonus_x_increase')
+      else:
+        self.player.score += 500
+
+  def r_joker_score(self, **kwargs):
+      if self.player.r_joker_on == True:
+        self.player.bonus_x += 1
+        self.player.bonus += 1000
+        self.update_bonus_lights()
+        self.joker_setup()
+        self.machine.events.post('bonus_x_increase')
+      else:
+        self.player.score += 500
+
+  def joker_setup(self):
+
+      #first turn all the joker lights off
+      self.machine.lights.l_lower_left_joker.off()
+      self.player.ll_joker_on = False
+      self.machine.lights.l_middle_left_joker.off()
+      self.player.ml_joker_on = False
+      self.machine.lights.l_top_left_joker.off()
+      self.player.ul_joker_on = False
+      self.machine.lights.l_right_joker.off()
+      self.player.r_joker_on = False
+      #picking a random number from 1 to 100, as an
+      #interger...
+      random_joker = random.randint(1,100)
+
+      #based on the random number, we will light one of
+      #the jokers
+      if random_joker <= 25:
+          self.player.ll_joker_on = True
+          self.machine.lights.l_lower_left_joker.on()
+      elif random_joker <= 50:
+          self.player.ml_joker_on = True
+          self.machine.lights.l_middle_left_joker.on()
+      elif random_joker <= 75:
+          self.player.ul_joker_on = True
+          self.machine.lights.l_top_left_joker.on()
+      else:
+          self.player.r_joker_on = True
+          self.machine.lights.l_right_joker.on()
+
+  def update_bonus_lights(self, **kwargs):
+      self.log.info("update bonus lights called")
+      bonusV = self.player.bonus
+      bonusX = self.player.bonus_x
+      #this is the remainder value after the big lights
+      #are subtracted (40k and 20k)
+      #set at 0 to start
+      bonusR = 0
+
+      bonus_lights = [ self.machine.lights.l_4k_bonus,
+                       self.machine.lights.l_6k_bonus,
+                       self.machine.lights.l_8k_bonus,
+                       self.machine.lights.l_10k_bonus,
+                       self.machine.lights.l_12k_bonus,
+                       self.machine.lights.l_14k_bonus,
+                       self.machine.lights.l_16k_bonus,
+                       self.machine.lights.l_18k_bonus ]
+
+      bonus_x_lights = [ self.machine.lights.l_2x,
+                         self.machine.lights.l_3x,
+                         self.machine.lights.l_4x,
+                         self.machine.lights.l_5x ]
+     #first we turn them all off
+      self.machine.lights.l_20k_bonus.off()
+      self.machine.lights.l_40k_bonus.off()
+      #turn off bonus X lights
+      for lamps in bonus_x_lights:
+          lamps.off()
+      #now turn off the bonus counter lights
+      for lamps in bonus_lights:
+          lamps.off()
+
+      #now turn on the bonus X lights
+      if bonusX >= 5:
+          for lamps in bonus_x_lights:
+              lamps.on()
+      elif bonusX >= 2:
+          for x in range (0, bonusX - 1):
+              bonus_x_lights[x].on()
+      else:
+          pass
+
+
+      #the maximum value of the bonus lights is 78,000 points
+      # (18k + 20k + 40k), so we take care of the top values first
+      if bonusV >= 78000:
+          self.machine.lights.l_20k_bonus.on()
+          self.machine.lights.l_40k_bonus.on()
+          for lamps in bonus_lights:
+              lamps.on()
+      elif bonusV >= 60000:
+          self.machine.lights.l_20k_bonus.on()
+          self.machine.lights.l_40k_bonus.on()
+          bonusR = bonusV - 60000
+      elif bonusV >= 40000:
+          self.machine.lights.l_40k_bonus.on()
+          bonusR = bonusV - 40000
+      elif bonusV >= 20000:
+          self.machine.lights.l_20k_bonus.on()
+          bonusR = bonusV - 20000
+      else:
+          bonusR = bonusV
+      #after the main lights are lit, the remainder is used
+      #to light up the rest of the bonus lights (4k to 18k)
+      if bonusR >= 4000:
+          lampcounter = (bonusR // 2000) - 1
+          for x in range (0, lampcounter):
+              bonus_lights[x].on()
--- modes/poker/code/poker.py
+++ modes/poker/code/poker.py
@@ -17,10 +17,13 @@ class poker(Mode):
         self.player.heart_eject_made = False
         self.player.club_eject_made = False
         self.player.spade_eject_made = False
+        self.player.ejects_remaining = 3
         #for now, start with one poker light
         self.player.poker_lights = 1
         self.player.poker_extra_ball_lit = False
+        self.player.poker_extra_ball_warning = False
         self.player.special_lit = False
+        self.player.special_lit_warning = False
         self.player.poker_extra_ball_awarded = False
         self.player.special_awarded = False
 
@@ -30,8 +33,7 @@ class poker(Mode):
         self.add_mode_event_handler("s_heart_ace_eject_active", self.heart_score)
         self.add_mode_event_handler("s_left_special_active", self.special_award)
         self.add_mode_event_handler("s_right_special_active", self.special_award)
-        self.add_mode_event_handler("ball started", self.poker_light_update)
-
+        self.add_mode_event_handler("ball_started", self.poker_light_update)
         #when mode first starts, we update the lights
         self.poker_light_update()
 
@@ -54,29 +56,34 @@ class poker(Mode):
             self.machine.events.post('spade_eject_solid')
         else:
             self.machine.events.post('spade_eject_flash')
-
+        #updates the extra ball lits, either flash, solid, or off
         if (self.player.poker_extra_ball_lit == True and self.player.poker_extra_ball_awarded == False):
             self.machine.events.post('extra_ball_flash')
         elif (self.player.poker_extra_ball_lit == False and self.player.poker_extra_ball_awarded == True):
             self.machine.events.post('extra_ball_solid')
         else:
             self.machine.events.post('extra_ball_off')
-
+       #updates the special lights
         if self.player.special_lit == True:
             self.machine.events.post('special_lit')
         else:
             self.machine.events.post('special_off')
-
+        #updates the shoot again light
+        if self.player.extra_balls >= 1:
+            self.machine.events.post('shoot_again_lit')
+        else:
+            self.machine.events.post('shoot_again_off')
+        #last we update the poker lights
         poker_light_call = 'poker_light_' + str(self.player.poker_lights)
         self.machine.events.post(poker_light_call)
 
     def heart_score(self, **kwargs):
         if self.player.heart_eject_made == False:
            self.player.heart_eject_made = True
+           self.player.ejects_remaining -= 1
            self.player.score += 10000
-           #we dont off the light show here
-           #because it is done in poker_check
-           self.poker_check()
+           if self.player.ejects_remaining > 0:
+              self.machine.events.post('eject_remain_slide')
         else:
            self.player.score +=1000
         #if there is an xtra ball lit, now is the time to award it
@@ -86,14 +93,17 @@ class poker(Mode):
            self.player.poker_extra_ball_awarded = True
            self.poker_light_update()
 
+        self.poker_check()
+        self.player.bonus +=3000
+
 
     def club_score(self, **kwargs):
         if self.player.club_eject_made == False:
             self.player.club_eject_made = True
+            self.player.ejects_remaining -= 1
+            if self.player.ejects_remaining > 0:
+               self.machine.events.post('eject_remain_slide')
             self.player.score += 10000
-             #we dont off the light show here
-             #because it is done in poker_check
-            self.poker_check()
         else:
             self.player.score += 1000
 
@@ -104,19 +114,32 @@ class poker(Mode):
            self.player.poker_extra_ball_awarded = True
            self.poker_light_update()
 
+        self.poker_check()
+        self.player.bonus += 3000
+
 
     def spade_score(self, **kwargs):
         if self.player.spade_eject_made == False:
             self.player.spade_eject_made = True
             self.player.score += 10000
+            self.player.ejects_remaining -= 1
+            if self.player.ejects_remaining > 0:
+                self.machine.events.post('eject_remain_slide')
              #we dont off the light show here
              #because it is done in poker_check
-            self.poker_check()
         else:
             self.player.score += 1000
 
+        self.poker_check()
+        self.player.bonus += 3000
+
     def special_award(self, **kwargs):
-        pass
+        if self.player.special_lit == True:
+            self.machine.events.post('special_award_eb')
+            self.player.special_lit = False
+            self.poker_light_update()
+        #regardless, we award bonus for outlanes
+        self.player.bonus += 3000
 
     def poker_check(self):
         self.log.info("Poker Check called")
@@ -134,11 +157,23 @@ class poker(Mode):
             self.player.heart_eject_made = False
             self.player.club_eject_made = False
             self.player.spade_eject_made = False
+            self.player.ejects_remaining = 3
             #now we check to see if we need to light up extra ball, or special
 
         #check for lit extra ball
         if (self.player.poker_lights == 3 and self.player.poker_extra_ball_awarded == False):
                  #extra ball is lit
             self.player.poker_extra_ball_lit = True
+            #need to tell player extra ball is lit, it only does this once
+            if self.player.poker_extra_ball_awarded == False:
+               self.machine.events.post('extra_ball_lit_slide')
+               self.player.poker_extra_ball_warning = True
+        #light special, lits up a 5 poker hits
+        if (self.player.poker_lights == 5 and self.player.special_awarded == False):
+            self.player.special_lit = True
+            #tell player special is lit
+            if self.player.special_lit_warning == False:
+               self.machine.events.post('special_lit_slide')
+               self.player.special_lit_warning = True
         #last step, update lights
         self.poker_light_update()
